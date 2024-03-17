082f587aae339fb177a64f5c905af203ad49c5ae
Xnths <jonathascastilhoes@gmail.com>
2024-03-17 07:20:56 -0300

    feat: Adicionando arquivos criado pelo spring

---
+++ api/src/test/kotlin/usp/yggdrasil/YggdrasilApplicationTests.kt
@@ -0,0 +1,13 @@
+package usp.yggdrasil
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YggdrasilApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
