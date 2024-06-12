7eceeb98f3c138c7649eef1b02e861255dca424f
Anna <annabserikyaku@gmail.com>
2024-06-12 17:49:03 -0300

    feat: configurando rotas para chamada da api

--- api/src/main/kotlin/usp/routes/MateriaRoutes.kt
+++ api/src/main/kotlin/usp/routes/MateriaRoutes.kt
@@ -3,18 +3,48 @@ package usp.routes
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.request.receive
+import io.ktor.http.HttpStatusCode
 import usp.models.materias
 import usp.repository.MateriaRepository
 import usp.repository.MySQLMateriaRepository
+import usp.models.Materia
 
-fun Route.materia() {
+fun Route.materiaRoute() {
     route("/materia") {
         val repository: MateriaRepository = MySQLMateriaRepository()
 
         get {
-            if (materias.isNotEmpty()) {
-                call.respond(materias)
+            call.respond(repository.getAllMaterias())
+        }
+
+        get("{codigo}") {
+            val codigo = call.parameters["codigo"]
+
+            if (codigo == null) {
+                call.respond(
+                    HttpStatusCode.BadRequest,
+                    "Código não fornecido"
+                )
+                return@get
             }
+
+            val materia = repository.getMateriasById(codigo)
+
+            if (materia == null) {
+                call.respond(
+                    HttpStatusCode.NotFound,
+                    "Disciplina com código $codigo não encontrada"
+                )
+            } else {
+                call.respond(materia)
+            }
+        }
+
+        post {
+            val materia = call.receive<Materia>()
+            repository.addMateria(materia)
+            call.respond(HttpStatusCode.Created, materia)
         }
     }
 }
