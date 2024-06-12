95f2eb8ef34e31258abf17a8a07e76796d925ca0
Anna <annabserikyaku@gmail.com>
2024-06-12 17:45:15 -0300

    feat: chamando rotas

--- api/src/main/kotlin/usp/plugins/Routing.kt
+++ api/src/main/kotlin/usp/plugins/Routing.kt
@@ -4,9 +4,11 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import usp.routes.alunoRoutes
+import usp.routes.materiaRoute
 
 fun Application. configureRouting() {
     routing {
         alunoRoutes()
+        materiaRoute();
     }
 }
