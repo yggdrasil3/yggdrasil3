c8a854d2cec1622c33ecc63da344404337250459
Anna <annabserikyaku@gmail.com>
2024-06-12 17:43:52 -0300

    refactor: mundando o nome da variavel

--- api/src/main/kotlin/usp/database/MateriaEntities.kt
+++ api/src/main/kotlin/usp/database/MateriaEntities.kt
@@ -4,7 +4,7 @@ import org.ktorm.schema.*
 import org.ktorm.entity.Entity
 import usp.database.DBMateriaTable.bindTo
 
-object DBMateriaTable: Table<>("materia") {
+object DBMateriaTable: Table<DBMateriaEntity>("materia") {
     val codigo = varchar("codigo").primaryKey().bindTo { it.codigo }
     val tipo = varchar("tipo").bindTo { it.tipo }
     val creditoAula = int("creditoAula").bindTo { it.creditoAula }
@@ -15,7 +15,7 @@ object DBMateriaTable: Table<>("materia") {
 interface DBMateriaEntity: Entity<DBMateriaEntity> {
     companion object : Entity.Factory<DBMateriaEntity>()
 
-    val codigo: Int
+    val codigo: String
     val tipo: String
     val creditoAula: Int
     val creditoTrabalho: Int
